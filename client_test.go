fa53e73efed871794fa564da41ed438dbe3b9a6f
Jon Wear <jon@dramafever.com>
2015-05-14 14:40:00 -0400

    added createjob test

--- client_test.go
+++ client_test.go
@@ -2,22 +2,34 @@ package gosail
 
 import (
 	"flag"
+	"log"
 	"testing"
 )
 
-var apikey = flag.String("apikey", "", "API Key for SailThru")
-var secretkey = flag.String("secretkey", "", "Secret Key for SailThru")
+var apiKey = flag.String("apikey", "", "API Key for SailThru")
+var secretKey = flag.String("secretkey", "", "Secret Key for SailThru")
 
 func checkKeys(t *testing.T) {
-	if *apikey == "" {
+	if *apiKey == "" {
 		t.Error(`Missing or blank required flag "-apikey={key}"`)
 	}
-	if *secretkey == "" {
+	if *secretKey == "" {
 		t.Error(`Missing or blank required flag "-secretkey={key}"`)
 	}
 }
 
 func TestFlag(t *testing.T) {
 	checkKeys(t)
+}
 
+func TestCreateJob(t *testing.T) {
+	sc := NewSailThruClient(*apiKey, *secretKey)
+	resp, err := sc.CreateJob("export_list_data", "ad_hoc_test_list_1", "json")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.JobID == "" {
+		log.Println(resp)
+		t.Errorf("JobID should not be empty string")
+	}
 }
